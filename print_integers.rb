608d09cd1e5a34519caada7b981729a3d1c6473a
Pam Willenz <pam@positek.net>
2021-02-24 13:52:43 -0500

    print integers above and below length

---
+++ print_integers.rb
@@ -0,0 +1,14 @@
+# 1. Print the number of integers in an array that are above 
+# the given input and the number that are below, 
+# e.g. for the array [1, 5, 2, 1, 10] 
+# with input 6, print “above: 1, below: 4”.
+
+  integers = [1, 5, 2, 1, 10]
+  result_1 = integers.length + 1
+  puts result_1
+
+  integers = [1, 5, 2, 1, 10]
+  result_2 = puts integers.length - 1
+  puts result_2
+
+  puts "#{result_1}, " + "#{result_2}."
