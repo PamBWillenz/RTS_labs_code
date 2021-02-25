98b0b64b2feb53bf7dfc9830cc90b47898f5e458
Pam Willenz <pam@positek.net>
2021-02-25 12:10:20 -0500

    change favorite_stack to md

--- favorite_stack_pick.rb
+++ favorite_stack_pick.rb
@@ -1,8 +1,8 @@
-# 3 If you could change 1 thing about your favorite 
+# 3. If you could change 1 thing about your favorite 
 # framework/language/platform (pick one), what would it be?
 
 # *************** ANSWER *****************
 
-# I pick Ruby on Rails as my favorite framework/language/platform. 
-# I wish RoR was had a gem that optimized the frontend better than bootstrap
-# (think hotwire) without relying on JS frameworks.
+I pick Ruby on Rails as my favorite framework/language/platform. 
+I wish RoR had a gem that optimized the frontend better than bootstrap
+(think hotwire) without relying on JS frameworks.
