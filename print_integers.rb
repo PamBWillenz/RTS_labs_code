427da2743aab0a6d252204d5ca928f9e34f77193
Pam Willenz <pam@positek.net>
2021-02-25 18:50:56 -0500

    redo print#integers exercise, need to make it dynamic

--- print_integers.rb
+++ print_integers.rb
@@ -2,15 +2,40 @@
 # the given input and the number that are below, 
 # e.g. for the array [1, 5, 2, 1, 10] 
 # with input 6, print “above: 1, below: 4”.
+class ArraySort
+  def initialize(array)
+    @array = array
+  end
 
-  integers = [1, 5, 2, 1, 10]
-  result_1 = integers.length + 1
-  result_2 = integers.length - 1
+  def above(input)
+    above = []
+    @array.each do |integer|
+      above << integer if integer > input
+    end
+    above.count
+  end
+
+  def below(input)
+    below = []
+    @array.each do |integer|
+      below << integer if integer < input
+    end
+    below.count
+  end
+
+  def numbers(input)
+    puts "above: #{self.above(input)} below: #{self.below(input)}"
+  end
+end
   
-  result_3 = "above: #{result_1}," " " "below: #{result_2}"
-  puts result_3
 
-  # => above: 6, below: 4
+  array_sort = 
+  ArraySort.new([1, 5, 10, 15, 20, 25, 30, 35, 36, 37, 38, 40, 42, 2, 1, 10])
+  
+  array_sort.numbers(6)
+  array_sort.numbers(3)
+  array_sort.numbers(10)
 
-
-
+  # => above: 12 below: 4
+  # => above: 13 below: 3
+  # => above: 10 below: 4
